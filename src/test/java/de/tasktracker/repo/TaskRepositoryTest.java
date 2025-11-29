18d8af8371a04801570dea579ace3f6265e0272b
Kamyab Kei Davoudi <keidavodi@stud.hs-heilbronn.de>
2025-11-30 00:28:29 +0100

    Add failing test for listing all tasks (RED - US2)

--- src/test/java/de/tasktracker/repo/TaskRepositoryTest.java
+++ src/test/java/de/tasktracker/repo/TaskRepositoryTest.java
@@ -3,6 +3,7 @@ package de.tasktracker.repo;
 import de.tasktracker.model.Task;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 public class
 TaskRepositoryTest {
@@ -29,5 +30,23 @@ TaskRepositoryTest {
             repo.add(newTask);
         });
     }
+
+    @Test
+    public void testGetAllTasksReturnsAllSavedTasks() {
+        TaskRepository repo = new InMemoryTaskRepository();
+
+        Task t1 = new Task("Aufgabe 1", "eins");
+        Task t2 = new Task("Aufgabe 2", "zwei");
+
+        repo.add(t1);
+        repo.add(t2);
+
+        List<Task> tasks = repo.getAllTasks();
+
+        assertEquals(2, tasks.size());
+        assertTrue(tasks.stream().anyMatch(t -> t.getTitle().equals("Aufgabe 1")));
+        assertTrue(tasks.stream().anyMatch(t -> t.getTitle().equals("Aufgabe 2")));
+    }
+
 
 }
